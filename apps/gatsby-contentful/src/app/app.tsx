00fe494abecdfd18bb8080cb06dc425255f24b94
Mauro <mauroeavellaneda@gmail.com>
2025-05-10 17:46:48 +0200

    Update app.tsx

--- apps/gatsby-contentful/src/app/app.tsx
+++ apps/gatsby-contentful/src/app/app.tsx
@@ -1,9 +1,74 @@
-import NxWelcome from './nx-welcome';
+import { useEffect, useState } from 'react';
+import { sanityClient } from '../sanity';
+import EventCard from './EventCard';
+
+export interface IEvent {
+  name: string;
+  slug: string;
+  eventType: string;
+  date: string;
+  doorsOpen: number;
+  headline: {
+    name: string;
+  };
+  image: {
+    asset: {
+      id: string;
+      url: string;
+    };
+  };
+  details: string[];
+  tickets: string;
+}
 
 export function App() {
+  const [events, setEvents] = useState<IEvent[] | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function fetchEventsFromSanity() {
+      try {
+        const data = await sanityClient.fetch(`*[_type == "event"]{
+          name,
+          slug,
+          eventType,
+          date,
+          doorsOpen,
+          headline->{
+            name
+          },
+          image{
+            asset->{
+              id,
+              url
+            }
+          },
+        }`);
+        setEvents(data);
+      } catch (err) {
+        setError(`Failed to fetch events: ${err}`);
+      }
+    }
+
+    fetchEventsFromSanity();
+  }, []);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
     <div>
-      <NxWelcome title="gatsby-contentful" />
+      {events && events.length > 0 ? (
+        events.map((event) => (
+          <EventCard
+            key={event.slug || event.date || event.name}
+            event={event}
+          />
+        ))
+      ) : (
+        <p>No events found.</p>
+      )}
     </div>
   );
 }
