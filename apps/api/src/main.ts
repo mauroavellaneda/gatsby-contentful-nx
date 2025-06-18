18ae5541953ebf204fc5332f58fef3134db0715e
Mauro <mauroeavellaneda@gmail.com>
2025-06-18 13:26:53 +0200

    Update main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 import cors from 'cors';
+import router from './routes/auth';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const app = express();
-
+app.use(express.json());
 app.use(cors());
+
+app.use('/auth', router);
+
 
 app.get('/', (req, res) => {
   res.send({ message: 'Hello API' });
