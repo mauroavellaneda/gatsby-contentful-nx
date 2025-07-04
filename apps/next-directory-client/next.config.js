7a5d43953233f6a98d99244406609fd00b17f2d1
Mauro <mauroeavellaneda@gmail.com>
2025-07-04 15:24:31 +0200

    Update next.config.js

--- apps/next-directory-client/next.config.js
+++ apps/next-directory-client/next.config.js
@@ -1,5 +1,3 @@
-import tailwindcss from '@tailwindcss/vite';
-
 const { composePlugins, withNx } = require('@nx/next');
 
 /**
@@ -14,7 +12,6 @@ const nextConfig = {
 const plugins = [
   // Add more Next.js plugins to this list if needed.
   withNx,
-  tailwindcss(),
 ];
 
 module.exports = composePlugins(...plugins)(nextConfig);
