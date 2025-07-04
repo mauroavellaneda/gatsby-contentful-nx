bf0c007ed0dcbaeda6ae6030473b9b4e1fec1d84
Mauro <mauroeavellaneda@gmail.com>
2025-07-04 13:14:54 +0200

    add inventory client

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,12 @@ module.exports = [
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
   {
-    ignores: ['**/dist'],
+    ignores: [
+      '**/dist',
+      '**/vite.config.*.timestamp*',
+      '**/vitest.config.*.timestamp*',
+      '**/test-output',
+    ],
   },
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
