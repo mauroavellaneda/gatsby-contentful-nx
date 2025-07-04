9aab248d6a29ac2465fcbde5bb442830b89247b3
Mauro <mauroeavellaneda@gmail.com>
2025-07-04 13:37:38 +0200

    remove inventory and create next js

--- apps/next-directory-client-e2e/playwright.config.ts
+++ apps/next-directory-client-e2e/playwright.config.ts
@@ -3,7 +3,7 @@ import { nxE2EPreset } from '@nx/playwright/preset';
 import { workspaceRoot } from '@nx/devkit';
 
 // For CI, you may want to set BASE_URL to the deployed application.
-const baseURL = process.env['BASE_URL'] || 'http://localhost:4300';
+const baseURL = process.env['BASE_URL'] || 'http://localhost:3000';
 
 /**
  * Read environment variables from file.
@@ -24,8 +24,8 @@ export default defineConfig({
   },
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npx nx run inventory-client:preview',
-    url: 'http://localhost:4300',
+    command: 'npx nx run next-directory-client:start',
+    url: 'http://localhost:3000',
     reuseExistingServer: true,
     cwd: workspaceRoot,
   },
